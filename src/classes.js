4f12bc23d3478cb88c887a741a41e88413339a7d
zhangxu <raingxm@163.com>
2016-06-11 23:26:14 +0800

    static method

--- src/classes.js
+++ src/classes.js
@@ -6,12 +6,17 @@ class User {
     this.email = email;
   }
 
+  static register(username, email) {
+    return new User(username, email);
+  }
+
   changeEmail(newEmail) {
     this.email = newEmail;
   }
 }
 
-let me = new User('zhangxu', 'raingxm@gmail.com');
-me.changeEmail('raingxm@163.com');
+let me = User.register('zhangxu1', 'raingxm@gmail.com');
+// let me = new User('zhangxu', 'raingxm@gmail.com');
+// me.changeEmail('raingxm@163.com');
 
 console.dir(me);
